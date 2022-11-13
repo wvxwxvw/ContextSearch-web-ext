4d451799b96311135dba225627c7ff326f82df75
ssborbis <ssborbis@gmail.com>
2022-11-12 19:31:32 -0600

    replace google reverse image search with google lens cleanup

--- src/defaultEngines.js
+++ src/defaultEngines.js
@@ -1,7 +1,7 @@
 var defaultEngines = [
 	{
 		"searchForm": "https://www.google.com/",
-		"template": "https://www.google.com/search?q={searchTerms}",
+		"template": "https://www.google.com/search?q=%s",
 		"icon_url": "http://google.com/favicon.ico",
 		"title": "Google",
 		"icon_base64String": "",
@@ -12,7 +12,7 @@ var defaultEngines = [
 	},
 	{
 		"searchForm": "https://www.amazon.com/",
-		"template": "https://www.amazon.com/exec/obidos/external-search/?field-keywords={searchTerms}&ie={inputEncoding}&mode=blended",
+		"template": "https://www.amazon.com/exec/obidos/external-search/?field-keywords=%s&ie={inputEncoding}&mode=blended",
 		"icon_url": "https://www.amazon.com/favicon.ico",
 		"title": "Amazon.com",
 		"icon_base64String": "",
@@ -23,7 +23,7 @@ var defaultEngines = [
 	},
 	{
 		"searchForm": "https://www.facebook.com/",
-		"template": "https://www.facebook.com/search/top/?q={searchTerms}&opensearch=1",
+		"template": "https://www.facebook.com/search/top/?q=%s&opensearch=1",
 		"icon_url": "http://facebook.com/favicon.ico",
 		"title": "Facebook",
 		"icon_base64String": "",
@@ -34,7 +34,7 @@ var defaultEngines = [
 	},
 	{
 		"searchForm": "http://www.youtube.com",
-		"template": "http://www.youtube.com/results?search_type=search_videos&search_query={searchTerms}&search_sort=relevance&search_category=0&page={startPage?}",
+		"template": "http://www.youtube.com/results?search_type=search_videos&search_query=%s&search_sort=relevance&search_category=0&page={startPage?}",
 		"icon_url": "http://youtube.com/favicon.ico",
 		"title": "YouTube",
 		"icon_base64String": "",
@@ -45,7 +45,7 @@ var defaultEngines = [
 	},
 	{
 		"searchForm": "https://twitter.com/",
-		"template": "https://twitter.com/search?q={searchTerms}&source=desktop-search",
+		"template": "https://twitter.com/search?q=%s&source=desktop-search",
 		"icon_url": "http://twitter.com/favicon.ico",
 		"title": "Twitter",
 		"icon_base64String": "",
@@ -56,7 +56,7 @@ var defaultEngines = [
 	},
 	{
 		"searchForm": "http://www.pinterest.com/search/pins/",
-		"template": "http://www.pinterest.com/search/pins/?q={searchTerms}",
+		"template": "http://www.pinterest.com/search/pins/?q=%s",
 		"icon_url": "http://pinterest.com/favicon.ico",
 		"title": "Pinterest",
 		"icon_base64String": "",
@@ -67,7 +67,7 @@ var defaultEngines = [
 	},
 	{
 		"searchForm": "https://www.bing.com/",
-		"template": "https://www.bing.com/search?q={searchTerms}",
+		"template": "https://www.bing.com/search?q=%s",
 		"icon_url": "http://bing.com/favicon.ico",
 		"title": "Bing",
 		"icon_base64String": "",
@@ -78,7 +78,7 @@ var defaultEngines = [
 	},
 	{
 		"searchForm": "https://duckduckgo.com/",
-		"template": "https://duckduckgo.com/?q={searchTerms}",
+		"template": "https://duckduckgo.com/?q=%s",
 		"icon_url": "http://duckduckgo.com/favicon.ico",
 		"title": "DuckDuckGo",
 		"icon_base64String": "",
@@ -89,7 +89,7 @@ var defaultEngines = [
 	},
 	{
 		"searchForm": "https://www.ebay.com/",
-		"template": "https://www.ebay.com/sch/i.html?_nkw={searchTerms}",
+		"template": "https://www.ebay.com/sch/i.html?_nkw=%s",
 		"icon_url": "http://ebay.com/favicon.ico",
 		"title": "eBay",
 		"icon_base64String": "",
@@ -100,7 +100,7 @@ var defaultEngines = [
 	},
 	{
 		"searchForm": "https://en.wikipedia.org/",
-		"template": "https://en.wikipedia.org/wiki/Special:Search?search={searchTerms}",
+		"template": "https://en.wikipedia.org/wiki/Special:Search?search=%s",
 		"icon_url": "http://wikipedia.org/favicon.ico",
 		"title": "Wikipedia (en)",
 		"icon_base64String": "",
@@ -110,10 +110,10 @@ var defaultEngines = [
 		"hidden": false
 	},
 	{
-		"searchForm": "https://images.google.com/",
-		"template": "https://www.google.com/searchbyimage?site=search&image_url={searchTerms}",
-		"icon_url": "http://icons.iconarchive.com/icons/designbolts/3d-social/256/Google-plus-icon.png",
-		"title": "Google Reverse Image Search",
+		"searchForm": "https://lens.google.com",
+		"template": "https://lens.google.com/uploadbyurl?url=%s",
+		"icon_url": "https://lh3.googleusercontent.com/AjRsGVa7hjknlPuBARCXO_tKGNVm8C-R6gsebEWCSwgcKfp2WPkdMVovsfK_tNbNwlbBfOqJCpHS2_ejJy4iEkeJtjHKE-Xfh9crQ9hGmNNMAOT0Ua8",
+		"title": "Google Lens",
 		"icon_base64String": "",
 		"method": "GET",
 		"params": [],
@@ -122,7 +122,7 @@ var defaultEngines = [
 	},
 	{
 		"searchForm": "https://www.google.com",
-		"template": "https://www.google.com/search?q=site%3A{selectdomain}+{searchTerms}",
+		"template": "https://www.google.com/search?q=site%3A%ds+%s",
 		"icon_url": "https://www.google.com/images/branding/product/ico/googleg_lodp.ico",
 		"title": "Google Site Search",
 		"icon_base64String": "",
